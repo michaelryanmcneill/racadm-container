578115247331cc8003fa3d458dd89150b7506d5b
michaelryanmcneill <michael@michaelryanmcneill.com>
2024-08-21 21:26:37 -0400

    Cleaning up stdout msg

--- fanctl.sh
+++ fanctl.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 MAX_TEMP=77
-GOOD_MSG="(II) Dell Fan Control"
 
 IPMI_BINARY=/usr/bin/ipmitool
 IPMI_PASSWORD_FILE=/run/secrets/idrac/token
@@ -18,13 +17,13 @@ CUR_TEMP=$(${IPMI_COMMAND} -c sdr type Temperature | grep '^Temp,' | cut -d',' -
 if [ ${CUR_TEMP} -ge ${MAX_TEMP} ]; then
 	# Set to auto mode
 	${IPMI_COMMAND} raw 0x30 0x30 0x01 0x01 > /dev/null 2>&1
-    GOOD_MSG="(!) High Temperature: ( ${CUR_TEMP}C >= ${MAX_TEMP}C ), reverting to automatic fan control"
+	echo "(!) High Temperature: ( ${CUR_TEMP}C >= ${MAX_TEMP}C ), reverting to automatic fan control"
 else
 	# Set to manual mode
 	${IPMI_COMMAND} raw 0x30 0x30 0x01 0x00 > /dev/null 2>&1
 	# Set to 1440rpm
 	${IPMI_COMMAND} raw 0x30 0x30 0x02 0x01 0x19 > /dev/null 2>&1
-    GOOD_MSG="(✓) Temperature OK: ( ${CUR_TEMP}C < ${MAX_TEMP}C ), setting fans to manual fan control (1440rpm)"
+	echo "(✓) Temperature OK: ( ${CUR_TEMP}C < ${MAX_TEMP}C ), setting fans to manual fan control (1440rpm)"
 fi
 
 RETVAL=$(($? + $RETVAL))
